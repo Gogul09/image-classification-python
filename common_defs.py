fde397615cc09e033eb767c98cdcb1f080a9e5d8
campisano <riccardo.campisano@gmail.com>
2018-08-14 01:14:56 -0300

    code updated to use python3, common defs file added, requirements.txt added

---
+++ common_defs.py
@@ -0,0 +1,41 @@
+import mahotas
+import cv2
+
+# fixed-sizes for image
+fixed_size = tuple((500, 500))
+
+# path to training data
+train_path = "dataset/train"
+
+# bins for histogram
+bins = 8
+
+
+# feature-descriptor-1: Hu Moments
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
+
+
+# feature-descriptor-2: Haralick Texture
+def fd_haralick(image):
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the haralick texture feature vector
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    # return the result
+    return haralick
+
+
+# feature-descriptor-3: Color Histogram
+def fd_histogram(image, mask=None):
+    # convert the image to HSV color-space
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # compute the color histogram
+    hist = cv2.calcHist(
+        [image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+    # normalize the histogram
+    cv2.normalize(hist, hist)
+    # return the histogram
+    return hist.flatten()
--- global.py
+++ global.py
@@ -6,54 +6,11 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-import mahotas
 import cv2
 import os
 import h5py
 
-# fixed-sizes for image
-fixed_size = tuple((500, 500))
-
-# path to training data
-train_path = "dataset/train"
-
-# no.of.trees for Random Forests
-num_trees = 100
-
-# bins for histogram
-bins = 8
-
-# train_test_split size
-test_size = 0.10
-
-# seed for reproducing same results
-seed = 9
-
-# feature-descriptor-1: Hu Moments
-def fd_hu_moments(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    feature = cv2.HuMoments(cv2.moments(image)).flatten()
-    return feature
-
-# feature-descriptor-2: Haralick Texture
-def fd_haralick(image):
-    # convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # compute the haralick texture feature vector
-    haralick = mahotas.features.haralick(gray).mean(axis=0)
-    # return the result
-    return haralick
-
-# feature-descriptor-3: Color Histogram
-def fd_histogram(image, mask=None):
-    # convert the image to HSV color-space
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # compute the color histogram
-    hist  = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
-    # normalize the histogram
-    cv2.normalize(hist, hist)
-    # return the histogram
-    return hist.flatten()
+from common_defs import fd_hu_moments, fd_haralick, fd_histogram, fixed_size, train_path
 
 # get the training labels
 train_labels = os.listdir(train_path)
@@ -82,9 +39,9 @@ for training_name in train_labels:
 
     k = 1
     # loop over the images in each sub-folder
-    for x in range(1,images_per_class+1):
+    for fname in os.listdir(dir):
         # get the image file name
-        file = dir + "/" + str(x) + ".jpg"
+        file = os.path.join(dir, fname)
 
         # read the image and resize it to a fixed-size
         image = cv2.imread(file)
@@ -108,30 +65,30 @@ for training_name in train_labels:
 
         i += 1
         k += 1
-    print "[STATUS] processed folder: {}".format(current_label)
+    print("[STATUS] processed folder: {}".format(current_label))
     j += 1
 
-print "[STATUS] completed Global Feature Extraction..."
+print("[STATUS] completed Global Feature Extraction...")
 
 # get the overall feature vector size
-print "[STATUS] feature vector size {}".format(np.array(global_features).shape)
+print("[STATUS] feature vector size {}".format(np.array(global_features).shape))
 
 # get the overall training label size
-print "[STATUS] training Labels {}".format(np.array(labels).shape)
+print("[STATUS] training Labels {}".format(np.array(labels).shape))
 
 # encode the target labels
 targetNames = np.unique(labels)
 le = LabelEncoder()
 target = le.fit_transform(labels)
-print "[STATUS] training labels encoded..."
+print("[STATUS] training labels encoded...")
 
 # normalize the feature vector in the range (0-1)
 scaler = MinMaxScaler(feature_range=(0, 1))
 rescaled_features = scaler.fit_transform(global_features)
-print "[STATUS] feature vector normalized..."
+print("[STATUS] feature vector normalized...")
 
-print "[STATUS] target labels: {}".format(target)
-print "[STATUS] target labels shape: {}".format(target.shape)
+print("[STATUS] target labels: {}".format(target))
+print("[STATUS] target labels shape: {}".format(target.shape))
 
 # save the feature vector using HDF5
 h5f_data = h5py.File('output/data.h5', 'w')
@@ -143,4 +100,4 @@ h5f_label.create_dataset('dataset_1', data=np.array(target))
 h5f_data.close()
 h5f_label.close()
 
-print "[STATUS] end of training.."
+print("[STATUS] end of training..")
